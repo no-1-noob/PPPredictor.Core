43a41f81c6aa4cf890f93d7089f1c98176f502b4
No.1 N[][]b <91905916+no-1-noob@users.noreply.github.com>
2025-09-13 12:22:08 +0200

    Development Project to Net8

--- PPPredictor.Core.Development/Program.cs
+++ PPPredictor.Core.Development/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualBasic;
 using PPPredictor.Core.API;
 using PPPredictor.Core.Calculator;
 using PPPredictor.Core.DataType;
 using PPPredictor.Core.DataType.BeatSaberEncapsulation;
 using PPPredictor.Core.DataType.Curve;
 using PPPredictor.Core.DataType.MapPool;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PPPredictor.Core.Development
 {
